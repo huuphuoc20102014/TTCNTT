bdcf5b5e31c15a9a3631eacc534e64e65958e006
Truong <truongpq197@gmail.com>
2019-11-01 00:06:39 +0700

    Finish Trang Chủ, Footer chưa

--- TTCNTT/TTCNTT/Controllers/HomeController.cs
+++ TTCNTT/TTCNTT/Controllers/HomeController.cs
@@ -26,10 +26,12 @@ namespace TTCNTT.Controllers
         {
             HomeViewModel model = new HomeViewModel();
             model.aboutus = await _dbContext.AboutUs.FirstOrDefaultAsync(h => h.Skill == "0");
-            model.listAboutUs = await _dbContext.AboutUs.Where(p => p.Skill == "1").ToListAsync();
+            model.listAboutUsSkill = await _dbContext.AboutUs.Where(p => p.Skill == "1").ToListAsync();
             model.listNews = await _dbContext.News.ToListAsync();
             model.listProduct = await _dbContext.Product.ToListAsync();
             model.listService = await _dbContext.Service.ToListAsync();
+            model.listCourse = await _dbContext.Course.ToListAsync();
+            model.listEmployee = await _dbContext.Employee.ToListAsync();
 
             return View(model);
         }
--- TTCNTT/TTCNTT/Models/HomeViewModel.cs
+++ TTCNTT/TTCNTT/Models/HomeViewModel.cs
@@ -9,10 +9,11 @@ namespace TTCNTT.Models
     public class HomeViewModel
     {
         public AboutUs aboutus { get; set; }
-        public List<AboutUs> listAboutUs { get; set; }
+        public List<AboutUs> listAboutUsSkill { get; set; }
         public List<News> listNews { get; set; }
         public List<Product> listProduct { get; set; }
         public List<Service> listService { get; set; }
         public List<Course> listCourse { get; set; }
+        public List<Employee> listEmployee { get; set; }
     }
 }
