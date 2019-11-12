bff73a32f94fa1c6a0da95ddfa6d376b1d697477
Truong <truongpq197@gmail.com>
2019-11-12 12:05:18 +0700

    NewsComment

---
+++ TTCNTT/ATAdmin/ATAdmin/Controllers/NewsCommentController.cs
@@ -0,0 +1,468 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ATAdmin.Efs.Entities;
+using FluentValidation;
+using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using ATAdmin.Efs.Context;
+
+namespace ATAdmin.Controllers
+{
+    //Microsoft.VisualStudio.Web.CodeGeneration.EntityFrameworkCore.NavigationMetadata
+    //FkNewsCommentType
+    //FkNewsCommentTypeId
+    public class NewsCommentController : AtBaseController
+    {
+        private readonly WebTTCNTTContext _context;
+
+        public NewsCommentController(WebTTCNTTContext context)
+        {
+            _context = context;
+        }
+
+        // GET: NewsComment
+        public async Task<IActionResult> Index([FromRoute]string id)
+        {
+            NewsComment dbItem = null;
+            if (!string.IsNullOrWhiteSpace(id))
+            {
+                dbItem = await _context.NewsComment.AsNoTracking().FirstOrDefaultAsync(h => h.Id == id);
+                if (dbItem == null)
+                {
+                    return NotFound();
+                }
+            }
+            ViewData["ParentItem"] = dbItem;
+
+            ViewData["ControllerNameForGrid"] = nameof(NewsCommentController).Replace("Controller", "");
+            return View();
+        }
+
+        public async Task<IActionResult> Index_Read([DataSourceRequest] DataSourceRequest request, string parentId)
+        {
+            var baseQuery = _context.NewsComment.AsNoTracking();
+            if (!string.IsNullOrWhiteSpace(parentId))
+            {
+                baseQuery = baseQuery.Where(h => h.Id == parentId);
+            }
+            var query = baseQuery
+                .Where(p => p.RowStatus == (int)AtRowStatus.Normal)
+                .Select(h => new NewsCommentDetailsViewModel
+                {
+                    Id = h.Id,
+                    FkNewsId = h.FkNewsId,
+                    // Ford
+                    Name = h.Name,
+                    Email = h.Email,
+                    Comment = h.Comment,
+                    IsRead = h.IsRead,
+                    CreatedBy = h.CreatedBy,
+                    CreatedDate = h.CreatedDate,
+                    UpdatedBy = h.UpdatedBy,
+                    UpdatedDate = h.UpdatedDate,
+                    RowVersion = h.RowVersion,
+                    RowStatus = (AtRowStatus)h.RowStatus,
+
+                });
+
+            return Json(await query.ToDataSourceResultAsync(request));
+        }
+
+
+        // GET: NewsComment/Details/5
+        public async Task<IActionResult> Details([FromRoute] string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comment = await _context.NewsComment.AsNoTracking()
+
+                .Include(n => n.FkNews)
+                    .Where(h => h.Id == id)
+                .FirstOrDefaultAsync();
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            return View(comment);
+        }
+
+        // GET: NewsComment/Create
+        public async Task<IActionResult> Create()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                ViewData["ControllerNameForImageBrowser"] = nameof(ImageBrowserNewController).Replace("Controller", "");
+                // Get list master of foreign property and set to view data
+                await PrepareListMasterForeignKey();
+
+                return View();
+            }
+            else
+            {
+                return RedirectToAction(nameof(ErrorController.Index), nameof(ErrorController).Replace("Controller", ""));
+            }
+        }
+
+        // POST: NewsComment/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([FromForm] NewsCommentCreateViewModel vmItem)
+        {
+            ViewData["ControllerNameForImageBrowser"] = nameof(ImageBrowserNewController).Replace("Controller", "");
+
+            // Invalid model
+            if (!ModelState.IsValid)
+            {
+                // Get list master of foreign property and set to view data
+                await PrepareListMasterForeignKey(vmItem);
+                return View(vmItem);
+            }
+
+            // Get time stamp for table to handle concurrency conflict
+            var tableName = nameof(NewsComment);
+            var tableVersion = await _context.TableVersion.FirstOrDefaultAsync(h => h.Id == tableName);
+
+            // Create save db item
+            var dbItem = new NewsComment
+            {
+                Id = Guid.NewGuid().ToString(),
+
+                CreatedBy = _loginUserId,
+                CreatedDate = DateTime.Now,
+                UpdatedBy = null,
+                UpdatedDate = null,
+                RowStatus = (int)AtRowStatus.Normal,
+                RowVersion = null,
+
+                FkNewsId = vmItem.FkNewsId,
+                Name = vmItem.Name,
+                Email = vmItem.Email,
+                Phone = vmItem.Phone,
+                Comment = vmItem.Comment,
+                IsRead = vmItem.IsRead,
+            };
+            _context.Add(dbItem);
+
+            // Set time stamp for table to handle concurrency conflict
+            if (tableVersion != null)
+            {
+                tableVersion.LastModify = DateTime.Now;
+            }
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Details), new { id = dbItem.Id });
+        }
+        
+        // GET: NewsComment/Edit/5
+        public async Task<IActionResult> Edit([FromRoute] string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+
+            var dbItem = await _context.NewsComment.AsNoTracking()
+                .Where(h => h.Id == id)
+                .Select(h => new NewsCommentEditViewModel
+                {
+                    Id = h.Id,
+                    FkNewsId = h.FkNewsId,
+                    Name = h.Name,
+                    Email = h.Email,
+                    Phone = h.Phone,
+                    Comment = h.Comment,
+                    IsRead = h.IsRead,
+                    RowVersion = h.RowVersion,
+                })
+                .FirstOrDefaultAsync();
+            if (dbItem == null)
+            {
+                return NotFound();
+            }
+
+            // Get list master of foreign property and set to view data
+            await PrepareListMasterForeignKey(dbItem);
+
+            return View(dbItem);
+        }
+
+        // POST: NewsComment/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit([FromForm] NewsCommentEditViewModel vmItem)
+        {
+
+            // Invalid model
+            if (!ModelState.IsValid)
+            {
+                // Get list master of foreign property and set to view data
+                await PrepareListMasterForeignKey(vmItem);
+                return View(vmItem);
+            }
+
+            // Get time stamp for table to handle concurrency conflict
+            var tableName = nameof(NewsComment);
+            var tableVersion = await _context.TableVersion.FirstOrDefaultAsync(h => h.Id == tableName);
+
+            var dbItem = await _context.NewsComment
+                .Where(h => h.Id == vmItem.Id)
+
+                .FirstOrDefaultAsync();
+            if (dbItem == null)
+            {
+                return NotFound();
+            }
+
+            // Trim white space
+
+
+
+            // Update db item               
+            dbItem.UpdatedBy = _loginUserId;
+            dbItem.UpdatedDate = DateTime.Now;
+            dbItem.RowVersion = vmItem.RowVersion;
+
+            dbItem.FkNewsId = vmItem.FkNewsId;
+            dbItem.Name = vmItem.Name;
+            dbItem.Email = vmItem.Email;
+            dbItem.Phone = vmItem.Phone;
+            dbItem.Comment = vmItem.Comment;
+            dbItem.IsRead = vmItem.IsRead;
+
+            _context.Entry(dbItem).Property(nameof(NewsComment.RowVersion)).OriginalValue = vmItem.RowVersion;
+            // Set time stamp for table to handle concurrency conflict
+            tableVersion.LastModify = DateTime.Now;
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Details), new { id = dbItem.Id });
+        }
+
+        // GET: NewsComment/Details/5
+        public async Task<IActionResult> Delete([FromRoute] string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dbItem = await _context.NewsComment.AsNoTracking()
+
+                .Include(n => n.FkNews)
+                    .Where(h => h.Id == id)
+                .FirstOrDefaultAsync();
+            if (dbItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(dbItem);
+        }
+
+        // POST: NewsComment/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete([FromForm] string id, [FromForm] byte[] rowVersion)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            // Get time stamp for table to handle concurrency conflict
+            var tableName = nameof(NewsComment);
+            var tableVersion = await _context.TableVersion.FirstOrDefaultAsync(h => h.Id == tableName);
+
+            var dbItem = await _context.NewsComment
+
+                .Include(n => n.FkNews)
+                .Where(h => h.Id == id)
+                .FirstOrDefaultAsync();
+            if (dbItem == null)
+            {
+                return NotFound();
+            }
+
+            if (rowVersion == null)
+            {
+                ModelState.AddModelError("RowVersion", "Invalid row version, please try again.");
+                return View(dbItem);
+            }
+
+            // Update db item               
+            if (dbItem.RowStatus != (int)AtRowStatus.Deleted)
+            {
+                dbItem.RowStatus = (int)AtRowStatus.Deleted;
+                dbItem.UpdatedBy = _loginUserId;
+                dbItem.UpdatedDate = DateTime.Now;
+                dbItem.RowVersion = rowVersion;
+
+                _context.Entry(dbItem).Property(nameof(NewsComment.RowVersion)).OriginalValue = rowVersion;
+                // Set time stamp for table to handle concurrency conflict
+                tableVersion.LastModify = DateTime.Now;
+                await _context.SaveChangesAsync();
+            }
+
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private async Task PrepareListMasterForeignKey(NewsCommentBaseViewModel vm = null)
+        {
+            ViewData["Fk_NewsId"] = new SelectList(
+                await _context.News.AsNoTracking()
+                    .Select(h => new { h.Id, h.Title, h.RowStatus})
+                    .Where(h=>h.RowStatus == (int)AtRowStatus.Normal)
+                    .OrderBy(h => h.Id)
+                    .ToListAsync(),
+                "Id", "Title", vm?.FkNewsId);
+
+                  }
+    }
+
+    public class ImageBrowserNewsCommentController : EditorImageBrowserController
+    {
+        public const string FOLDER_NAME = "ImagesNewsComment";
+        public string FOLDER_ROOTPATH;
+
+        /// <summary>
+        /// Gets the base paths from which content will be served.
+        /// </summary>
+        public override string ContentPath
+        {
+            get
+            {
+                return CreateUserFolder();
+            }
+        }
+
+        public ImageBrowserNewsCommentController(IHostingEnvironment hostingEnvironment, IConfiguration staticFileSetting)
+           : base(hostingEnvironment)
+        {
+            FOLDER_ROOTPATH = staticFileSetting.GetValue<string>("StaticFileSetting");
+        }
+        private string CreateUserFolder()
+        {
+            var virtualPath = System.IO.Path.Combine(FOLDER_NAME);
+            //var path = HostingEnvironment.WebRootFileProvider.GetFileInfo(virtualPath).PhysicalPath;
+            var path = System.IO.Path.Combine(FOLDER_ROOTPATH, FOLDER_NAME);
+            if (!System.IO.Directory.Exists(path))
+            {
+                System.IO.Directory.CreateDirectory(path);
+            }
+            return path;
+        }
+    }
+
+
+    public class NewsCommentBaseViewModel
+    {
+
+        public String FkNewsId { get; set; }
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public String Phone { get; set; }
+        public String Comment { get; set; }
+        public Boolean? IsRead { get; set; }
+    }
+
+    public class NewsCommentDetailsViewModel : NewsCommentBaseViewModel
+    {
+
+        public String Id { get; set; }
+        public String CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public String UpdatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public Byte[] RowVersion { get; set; }
+        public AtRowStatus RowStatus { get; set; }
+
+    }
+
+    public class NewsCommentCreateViewModel : NewsCommentBaseViewModel
+    {
+
+    }
+
+    public class NewsCommentEditViewModel : NewsCommentBaseViewModel
+    {
+
+        public String Id { get; set; }
+        public Byte[] RowVersion { get; set; }
+    }
+
+    public class NewsCommentBaseValidator<T> : AtBaseValidator<T> where T : NewsCommentBaseViewModel
+    {
+        public NewsCommentBaseValidator()
+        {
+            RuleFor(h => h.FkNewsId)
+                        .NotEmpty()
+                        .MaximumLength(50)
+                ;
+
+            RuleFor(h => h.Name)
+                        .MaximumLength(50)
+                ;
+
+            RuleFor(h => h.Email)
+                        .NotEmpty()
+                        .MaximumLength(50)
+                ;
+
+            RuleFor(h => h.Phone)
+                        .MaximumLength(20)
+                ;
+
+            RuleFor(h => h.Comment)
+                        .NotEmpty()
+                        .MaximumLength(500)
+                ;
+
+        }
+    }
+
+    public class NewsCommentCreateValidator : NewsCommentBaseValidator<NewsCommentCreateViewModel>
+    {
+        public NewsCommentCreateValidator()
+        {
+        }
+    }
+
+    public class NewsCommentEditValidator : NewsCommentBaseValidator<NewsCommentEditViewModel>
+    {
+        public NewsCommentEditValidator()
+        {
+            RuleFor(h => h.Id)
+                        .NotEmpty()
+                        .MaximumLength(50)
+                ;
+
+            RuleFor(h => h.RowVersion)
+                        .NotNull()
+                ;
+
+        }
+    }
+
+
+
+
+
+
+
+
+}
