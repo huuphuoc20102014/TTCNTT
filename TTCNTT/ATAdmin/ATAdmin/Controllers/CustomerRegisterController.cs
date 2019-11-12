7e14678ae7e2faa0ee8c297da532f5a8e1954e4c
Truong <truongpq197@gmail.com>
2019-11-12 09:25:00 +0700

    CustomerRegister

---
+++ TTCNTT/ATAdmin/ATAdmin/Controllers/CustomerRegisterController.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using ATAdmin.Efs.Entities;
+using FluentValidation;
+using ATAdmin.Efs.Context;
+
+namespace ATAdmin.Controllers
+{
+    public class CustomerRegisterController : AtBaseController
+    {
+        private readonly WebTTCNTTContext _context;
+
+        public CustomerRegisterController(WebTTCNTTContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CustomerRegister
+        public async Task<IActionResult> Index([FromRoute]string id)
+        {
+            CustomerRegister dbItem = null;
+            if (!string.IsNullOrWhiteSpace(id))
+            {
+                dbItem = await _context.CustomerRegister.AsNoTracking().FirstOrDefaultAsync(h => h.Id == id);
+                if (dbItem == null)
+                {
+                    return NotFound();
+                }
+            }
+            ViewData["ParentItem"] = dbItem;
+
+            ViewData["ControllerNameForGrid"] = nameof(CustomerRegisterController).Replace("Controller", "");
+            return View();
+        }
+
+        public async Task<IActionResult> Index_Read([DataSourceRequest] DataSourceRequest request, string parentId)
+        {
+            var baseQuery = _context.CustomerRegister.AsNoTracking();
+            if (!string.IsNullOrWhiteSpace(parentId))
+            {
+                baseQuery = baseQuery.Where(h => h.Id == parentId);
+            }
+            var query = baseQuery
+                .Where(p => p.RowStatus == (int)AtRowStatus.Normal)
+                .Select(h => new CustomerRegisterDetailsViewModel
+                {
+                    Id = h.Id,
+                    Email = h.Email,
+                    CreatedBy = h.CreatedBy,
+                    CreatedDate = h.CreatedDate,
+                    UpdatedBy = h.UpdatedBy,
+                    UpdatedDate = h.UpdatedDate,
+                    RowVersion = h.RowVersion,
+                    RowStatus = (AtRowStatus)h.RowStatus,
+
+                });
+
+            return Json(await query.ToDataSourceResultAsync(request));
+        }
+
+
+        // GET: CustomerRegister/Details/5
+        public async Task<IActionResult> Details([FromRoute] string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.CustomerRegister.AsNoTracking()
+
+                    .Where(h => h.Id == id)
+                .FirstOrDefaultAsync();
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // GET: CustomerRegister/Create
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        // POST: CustomerRegister/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([FromForm] CustomerRegisterCreateViewModel vmItem)
+        {
+
+            // Invalid model
+            if (!ModelState.IsValid)
+            {
+                return View(vmItem);
+            }
+
+            // Get time stamp for table to handle concurrency conflict
+            var tableName = nameof(CustomerRegister);
+            var tableVersion = await _context.TableVersion.FirstOrDefaultAsync(h => h.Id == tableName);
+
+            // Trim white space
+
+
+
+            // Create save db item
+            var dbItem = new CustomerRegister
+            {
+                Id = Guid.NewGuid().ToString(),
+
+                CreatedBy = _loginUserId,
+                CreatedDate = DateTime.Now,
+                UpdatedBy = null,
+                UpdatedDate = null,
+                RowStatus = (int)AtRowStatus.Normal,
+                RowVersion = null,
+
+                Email = vmItem.Email,
+            };
+            _context.Add(dbItem);
+
+            // Set time stamp for table to handle concurrency conflict
+            tableVersion.LastModify = DateTime.Now;
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Details), new { id = dbItem.Id });
+        }
+
+        // GET: CustomerRegister/Edit/5
+        public async Task<IActionResult> Edit([FromRoute] string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+
+            var dbItem = await _context.CustomerRegister.AsNoTracking()
+
+    .Where(h => h.Id == id)
+
+                .Select(h => new CustomerRegisterEditViewModel
+                {
+                    Id = h.Id,
+                    Email = h.Email,
+
+                    RowVersion = h.RowVersion,
+                })
+                .FirstOrDefaultAsync();
+            if (dbItem == null)
+            {
+                return NotFound();
+            }
+
+
+            return View(dbItem);
+        }
+
+        // POST: CustomerRegister/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit([FromForm] CustomerRegisterEditViewModel vmItem)
+        {
+
+            // Invalid model
+            if (!ModelState.IsValid)
+            {
+                return View(vmItem);
+            }
+
+            // Get time stamp for table to handle concurrency conflict
+            var tableName = nameof(CustomerRegister);
+            var tableVersion = await _context.TableVersion.FirstOrDefaultAsync(h => h.Id == tableName);
+
+            var dbItem = await _context.CustomerRegister
+                .Where(h => h.Id == vmItem.Id)
+
+                .FirstOrDefaultAsync();
+            if (dbItem == null)
+            {
+                return NotFound();
+            }
+
+            // Trim white space
+
+
+
+            // Update db item               
+            dbItem.UpdatedBy = _loginUserId;
+            dbItem.UpdatedDate = DateTime.Now;
+            dbItem.RowVersion = vmItem.RowVersion;
+
+            dbItem.Email = vmItem.Email;
+
+            _context.Entry(dbItem).Property(nameof(CustomerRegister.RowVersion)).OriginalValue = vmItem.RowVersion;
+            // Set time stamp for table to handle concurrency conflict
+            tableVersion.LastModify = DateTime.Now;
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Details), new { id = dbItem.Id });
+        }
+
+        // GET: CustomerRegister/Details/5
+        public async Task<IActionResult> Delete([FromRoute] string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dbItem = await _context.CustomerRegister.AsNoTracking()
+
+                    .Where(h => h.Id == id)
+                .FirstOrDefaultAsync();
+            if (dbItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(dbItem);
+        }
+
+        // POST: CustomerRegister/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete([FromForm] string id, [FromForm] byte[] rowVersion)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            // Get time stamp for table to handle concurrency conflict
+            var tableName = nameof(CustomerRegister);
+            var tableVersion = await _context.TableVersion.FirstOrDefaultAsync(h => h.Id == tableName);
+
+            var dbItem = await _context.CustomerRegister
+
+                .Where(h => h.Id == id)
+                .FirstOrDefaultAsync();
+            if (dbItem == null)
+            {
+                return NotFound();
+            }
+
+            if (rowVersion == null)
+            {
+                ModelState.AddModelError("RowVersion", "Invalid row version, please try again.");
+                return View(dbItem);
+            }
+
+            // Update db item               
+            if (dbItem.RowStatus != (int)AtRowStatus.Deleted)
+            {
+                dbItem.RowStatus = (int)AtRowStatus.Deleted;
+                dbItem.UpdatedBy = _loginUserId;
+                dbItem.UpdatedDate = DateTime.Now;
+                dbItem.RowVersion = rowVersion;
+
+                _context.Entry(dbItem).Property(nameof(CustomerRegister.RowVersion)).OriginalValue = rowVersion;
+                // Set time stamp for table to handle concurrency conflict
+                tableVersion.LastModify = DateTime.Now;
+                await _context.SaveChangesAsync();
+            }
+
+
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+
+
+
+    public class CustomerRegisterBaseViewModel
+    {
+
+        public String Email { get; set; }
+    }
+
+    public class CustomerRegisterDetailsViewModel : CustomerRegisterBaseViewModel
+    {
+
+        public String Id { get; set; }
+        public String CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public String UpdatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public Byte[] RowVersion { get; set; }
+        public AtRowStatus RowStatus { get; set; }
+
+
+    }
+
+    public class CustomerRegisterCreateViewModel : CustomerRegisterBaseViewModel
+    {
+
+    }
+
+    public class CustomerRegisterEditViewModel : CustomerRegisterBaseViewModel
+    {
+
+        public String Id { get; set; }
+        public Byte[] RowVersion { get; set; }
+    }
+
+    public class CustomerRegisterBaseValidator<T> : AtBaseValidator<T> where T : CustomerRegisterBaseViewModel
+    {
+        public CustomerRegisterBaseValidator()
+        {
+            RuleFor(h => h.Email)
+                        .NotEmpty()
+                        .MaximumLength(100)
+                ;
+
+
+        }
+    }
+
+    public class CustomerRegisterCreateValidator : CustomerRegisterBaseValidator<CustomerRegisterCreateViewModel>
+    {
+        public CustomerRegisterCreateValidator()
+        {
+        }
+    }
+
+    public class CustomerRegisterEditValidator : CustomerRegisterBaseValidator<CustomerRegisterEditViewModel>
+    {
+        public CustomerRegisterEditValidator()
+        {
+            RuleFor(h => h.Id)
+                        .NotEmpty()
+                        .MaximumLength(50)
+                ;
+
+            RuleFor(h => h.RowVersion)
+                        .NotNull()
+                ;
+
+        }
+    }
+
+
+
+
+
+
+
+
+}
