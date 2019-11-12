19c4a0ddcaee88ca257205021ce47d92e1257486
Truong <truongpq197@gmail.com>
2019-11-12 11:07:05 +0700

    Menu

---
+++ TTCNTT/ATAdmin/ATAdmin/Controllers/MenuController.cs
@@ -0,0 +1,428 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using FluentValidation;
+using ATAdmin.Efs.Context;
+using ATAdmin.Efs.Entities;
+using Menu = ATAdmin.Efs.Entities.Menu;
+
+namespace ATAdmin.Controllers
+{
+    public class MenuController : AtBaseController
+    {
+        private readonly WebTTCNTTContext _context;
+
+        public MenuController(WebTTCNTTContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Menu
+        public async Task<IActionResult> Index([FromRoute]string id)
+        {
+            Menu dbItem = null;
+            if (!string.IsNullOrWhiteSpace(id))
+            {
+                dbItem = await _context.Menu.AsNoTracking().FirstOrDefaultAsync(h => h.Id == id);
+                if (dbItem == null)
+                {
+                    return NotFound();
+                }
+            }
+            ViewData["ParentItem"] = dbItem;
+
+            ViewData["ControllerNameForGrid"] = nameof(MenuController).Replace("Controller", "");
+            return View();
+        }
+
+        public async Task<IActionResult> Index_Read([DataSourceRequest] DataSourceRequest request, string parentId)
+        {
+            var baseQuery = _context.Menu.AsNoTracking();
+            if (!string.IsNullOrWhiteSpace(parentId))
+            {
+                baseQuery = baseQuery.Where(h => h.Id == parentId);
+            }
+            var query = baseQuery
+                .Where(p => p.RowStatus == (int)AtRowStatus.Normal)
+                .Select(h => new MenuDetailsViewModel
+                {
+                    Id = h.Id,
+                    Name = h.Name,
+                    SlugName = h.Slug_Name,
+                    IsAdminMenu = h.IsAdminMenu,
+                    IsMainMenu = h.IsMainMenu,
+                    ImageSlug = h.ImageSlug,
+                    Tags = h.Tags,
+                    KeyWord = h.KeyWord,
+                    MetaData = h.MetaData,
+                    Note = h.Note,
+                    CreatedBy = h.CreatedBy,
+                    CreatedDate = h.CreatedDate,
+                    UpdatedBy = h.UpdatedBy,
+                    UpdatedDate = h.UpdatedDate,
+                    RowVersion = h.RowVersion,
+                    RowStatus = (AtRowStatus)h.RowStatus,
+
+                });
+
+            return Json(await query.ToDataSourceResultAsync(request));
+        }
+
+
+        // GET: Menu/Details/5
+        public async Task<IActionResult> Details([FromRoute] string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var menu = await _context.Menu.AsNoTracking()
+
+                    .Where(h => h.Id == id)
+                .FirstOrDefaultAsync();
+            if (menu == null)
+            {
+                return NotFound();
+            }
+
+            return View(menu);
+        }
+
+        // GET: Menu/Create
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        // POST: Menu/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([FromForm] MenuCreateViewModel vmItem)
+        {
+
+            // Invalid model
+            if (!ModelState.IsValid)
+            {
+                return View(vmItem);
+            }
+
+            // Get time stamp for table to handle concurrency conflict
+            var tableName = nameof(Menu);
+            var tableVersion = await _context.TableVersion.FirstOrDefaultAsync(h => h.Id == tableName);
+
+            // Trim white space
+
+
+
+            // Create save db item
+            var dbItem = new Menu
+            {
+                Id = Guid.NewGuid().ToString(),
+
+                CreatedBy = _loginUserId,
+                CreatedDate = DateTime.Now,
+                UpdatedBy = null,
+                UpdatedDate = null,
+                RowStatus = (int)AtRowStatus.Normal,
+                RowVersion = null,
+
+                Name = vmItem.Name,
+                Slug_Name = vmItem.SlugName,
+                AutoSlug = vmItem.AutoSlug,
+                IsAdminMenu = vmItem.IsAdminMenu,
+                IsMainMenu = vmItem.IsMainMenu,
+                ImageSlug = vmItem.ImageSlug,
+                Tags = vmItem.Tags,
+                KeyWord = vmItem.KeyWord,
+                MetaData = vmItem.MetaData,
+                Note = vmItem.Note,
+            };
+            _context.Add(dbItem);
+
+            // Set time stamp for table to handle concurrency conflict
+            tableVersion.LastModify = DateTime.Now;
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Details), new { id = dbItem.Id });
+        }
+
+        // GET: Menu/Edit/5
+        public async Task<IActionResult> Edit([FromRoute] string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+
+            var dbItem = await _context.Menu.AsNoTracking()
+
+    .Where(h => h.Id == id)
+
+                .Select(h => new MenuEditViewModel
+                {
+                    Id = h.Id,
+                    Name = h.Name,
+                    SlugName = h.Slug_Name,
+                    AutoSlug = h.AutoSlug,
+                    IsAdminMenu = h.IsAdminMenu,
+                    IsMainMenu = h.IsMainMenu,
+                    ImageSlug = h.ImageSlug,
+                    Tags = h.Tags,
+                    KeyWord = h.KeyWord,
+                    MetaData = h.MetaData,
+                    Note = h.Note,
+                    RowVersion = h.RowVersion,
+                })
+                .FirstOrDefaultAsync();
+            if (dbItem == null)
+            {
+                return NotFound();
+            }
+
+
+            return View(dbItem);
+        }
+
+        // POST: Menu/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit([FromForm] MenuEditViewModel vmItem)
+        {
+
+            // Invalid model
+            if (!ModelState.IsValid)
+            {
+                return View(vmItem);
+            }
+
+            // Get time stamp for table to handle concurrency conflict
+            var tableName = nameof(Menu);
+            var tableVersion = await _context.TableVersion.FirstOrDefaultAsync(h => h.Id == tableName);
+
+            var dbItem = await _context.Menu
+                .Where(h => h.Id == vmItem.Id)
+
+                .FirstOrDefaultAsync();
+            if (dbItem == null)
+            {
+                return NotFound();
+            }
+
+            // Trim white space
+
+
+
+            // Update db item               
+            dbItem.UpdatedBy = _loginUserId;
+            dbItem.UpdatedDate = DateTime.Now;
+            dbItem.RowVersion = vmItem.RowVersion;
+
+            dbItem.Name = vmItem.Name;
+            dbItem.Slug_Name = vmItem.SlugName;
+            dbItem.AutoSlug = vmItem.AutoSlug;
+            dbItem.IsAdminMenu = vmItem.IsAdminMenu;
+            dbItem.IsMainMenu = vmItem.IsMainMenu;
+            dbItem.ImageSlug = vmItem.ImageSlug;
+            dbItem.Tags = vmItem.Tags;
+            dbItem.KeyWord = vmItem.KeyWord;
+            dbItem.MetaData = vmItem.MetaData;
+            dbItem.Note = vmItem.Note;
+
+            _context.Entry(dbItem).Property(nameof(Menu.RowVersion)).OriginalValue = vmItem.RowVersion;
+            // Set time stamp for table to handle concurrency conflict
+            tableVersion.LastModify = DateTime.Now;
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Details), new { id = dbItem.Id });
+        }
+
+        // GET: Menu/Details/5
+        public async Task<IActionResult> Delete([FromRoute] string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dbItem = await _context.Menu.AsNoTracking()
+
+                    .Where(h => h.Id == id)
+                .FirstOrDefaultAsync();
+            if (dbItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(dbItem);
+        }
+
+        // POST: Menu/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete([FromForm] string id, [FromForm] byte[] rowVersion)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            // Get time stamp for table to handle concurrency conflict
+            var tableName = nameof(Menu);
+            var tableVersion = await _context.TableVersion.FirstOrDefaultAsync(h => h.Id == tableName);
+
+            var dbItem = await _context.Menu
+
+                .Where(h => h.Id == id)
+                .FirstOrDefaultAsync();
+            if (dbItem == null)
+            {
+                return NotFound();
+            }
+
+            if (rowVersion == null)
+            {
+                ModelState.AddModelError("RowVersion", "Invalid row version, please try again.");
+                return View(dbItem);
+            }
+
+            // Update db item               
+            if (dbItem.RowStatus != (int)AtRowStatus.Deleted)
+            {
+                dbItem.RowStatus = (int)AtRowStatus.Deleted;
+                dbItem.UpdatedBy = _loginUserId;
+                dbItem.UpdatedDate = DateTime.Now;
+                dbItem.RowVersion = rowVersion;
+
+                _context.Entry(dbItem).Property(nameof(Menu.RowVersion)).OriginalValue = rowVersion;
+                // Set time stamp for table to handle concurrency conflict
+                tableVersion.LastModify = DateTime.Now;
+                await _context.SaveChangesAsync();
+            }
+
+
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+
+
+
+    public class MenuBaseViewModel
+    {
+
+        public String Name { get; set; }
+        public String SlugName { get; set; }
+        public Boolean AutoSlug { get; set; }
+        public Boolean IsAdminMenu { get; set; }
+        public Boolean IsMainMenu { get; set; }
+        public String ImageSlug { get; set; }
+        public String IconSlug { get; set; }
+        public String Tags { get; set; }
+        public String KeyWord { get; set; }
+        public String MetaData { get; set; }
+        public String Note { get; set; }
+    }
+
+    public class MenuDetailsViewModel : MenuBaseViewModel
+    {
+
+        public String Id { get; set; }
+        public String CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public String UpdatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public Byte[] RowVersion { get; set; }
+        public AtRowStatus RowStatus { get; set; }
+
+
+    }
+
+    public class MenuCreateViewModel : MenuBaseViewModel
+    {
+
+    }
+
+    public class MenuEditViewModel : MenuBaseViewModel
+    {
+
+        public String Id { get; set; }
+        public Byte[] RowVersion { get; set; }
+    }
+
+    public class MenuBaseValidator<T> : AtBaseValidator<T> where T : MenuBaseViewModel
+    {
+        public MenuBaseValidator()
+        {
+            RuleFor(h => h.Name)
+                        .NotEmpty()
+                        .MaximumLength(500)
+                ;
+
+            RuleFor(h => h.SlugName)
+                        .NotEmpty()
+                        .MaximumLength(500)
+                ;
+
+            RuleFor(h => h.AutoSlug)
+                ;
+
+            RuleFor(h => h.ImageSlug)
+                        .MaximumLength(100)
+                ;
+
+            RuleFor(h => h.Tags)
+                        .MaximumLength(1000)
+                ;
+
+            RuleFor(h => h.KeyWord)
+                        .MaximumLength(1000)
+                ;
+
+            RuleFor(h => h.MetaData)
+                        .MaximumLength(1000)
+                ;
+
+            RuleFor(h => h.Note)
+                        .MaximumLength(1000)
+                ;
+
+        }
+    }
+
+    public class MenuCreateValidator : MenuBaseValidator<MenuCreateViewModel>
+    {
+        public MenuCreateValidator()
+        {
+        }
+    }
+
+    public class MenuEditValidator : MenuBaseValidator<MenuEditViewModel>
+    {
+        public MenuEditValidator()
+        {
+            RuleFor(h => h.Id)
+                        .NotEmpty()
+                        .MaximumLength(50)
+                ;
+
+            RuleFor(h => h.RowVersion)
+                        .NotNull()
+                ;
+
+        }
+    }
+}
